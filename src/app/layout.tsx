7535d1eaae07ae3f55ea974b341461617489ff46
Roben Manala <rcerezomartin@gmail.com>
2025-09-28 15:02:39 +0000

    sigo viendo lo mismo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
           {`
             if ('serviceWorker' in navigator) {
               window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/PhysikBreak/sw.js').then(registration => {
+                navigator.serviceWorker.register('/sw.js').then(registration => {
                   console.log('ServiceWorker registration successful with scope: ', registration.scope);
                 }, err => {
                   console.log('ServiceWorker registration failed: ', err);
